01238b6ac7f3e0b1c2ad48d6b875e4c7fcf5a37e
nohjunh <nsh042777@naver.com>
2022-09-23 23:47:39 +0900

    google login

--- app/src/main/java/com/example/howistagram_f16/LoginActivity.kt
+++ app/src/main/java/com/example/howistagram_f16/LoginActivity.kt
@@ -4,53 +4,111 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.Nullable
+import com.example.howistagram_f16.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import kotlinx.android.synthetic.main.activity_login.*
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
-    var auth : FirebaseAuth? = null
+    // 중요 !! 여기서는 activity_login.xml을 사용하므로 ActivityLoginBinding으로 설정함
+    private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
+    var auth: FirebaseAuth? = null
+    var googleSignInClient: GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        setContentView(binding.root)
+
         auth = FirebaseAuth.getInstance()
-        email_login_button.setOnClickListener{
+        binding.emailLoginButton.setOnClickListener {
             signinAndSignup()
         }
-    }
-    fun signinAndSignup(){
-        auth?.createUserWithEmailAndPassword(
-            email_edittext.text.toString(),
-            password_edittext.text.toString())?.addOnCompleteListener {
-                task->
-                    if(task.isSuccessful){
-                        //Creating a user account
-                        moveMainPage(task.result?.user)
-                    }else if(!task.exception?.message.isNullOrEmpty()){
-                        //Show the error message
-                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
-                    }else{
-                        //Login if you have account
-                        signinEmail()
-                    }
+        binding.googleSignInButton.setOnClickListener {
+            //First Step
+            googleLogin()
         }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
-    fun signinEmail(){
-        auth?.createUserWithEmailAndPassword(
-            email_edittext.text.toString(),
-            password_edittext.text.toString())?.addOnCompleteListener {
-                task->
-            if(task.isSuccessful){
-                // Login
-                moveMainPage(task.result?.user)
-            }else{
-                //Show the error message
-                Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+
+    fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // google login
+        if(requestCode == GOOGLE_LOGIN_CODE){
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data!!)
+            if (result != null) {
+                if(result.isSuccess){
+                    var account = result.signInAccount
+                    firebaseAuthWithGoogle(account!!)
+                }
             }
         }
     }
-    fun moveMainPage(user: FirebaseUser?){
-        if(user != null){
+
+    fun firebaseAuthWithGoogle(account:GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){
+                    // Login
+                    moveMainPage(task.result?.user)
+                }else{
+                    // Show the error message
+                    Toast.makeText(this, task.exception!!.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    fun signinAndSignup(){
+        auth?.createUserWithEmailAndPassword(binding.emailEdittext.text.toString(), binding.passwordEdittext.text.toString())
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){
+                    // Creating a user account
+                    moveMainPage(task.result?.user)
+                }else if(task.exception?.message.isNullOrEmpty()){
+                    // Show the error message
+                    Toast.makeText(this, task.exception!!.message, Toast.LENGTH_LONG).show()
+                }else{
+                    // Login if you have account
+                    signinEmail()
+                }
+            }
+    }
+
+    fun signinEmail(){
+        auth?.signInWithEmailAndPassword(binding.emailEdittext.text.toString(), binding.passwordEdittext.text.toString())
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){
+                    // Login
+                    moveMainPage(task.result?.user)
+                }else{
+                    // Show the error message
+                    Toast.makeText(this, task.exception!!.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    fun moveMainPage(user:FirebaseUser?){
+        if(user!=null){
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
