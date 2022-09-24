86ef96a1962bab5faadade74bcb6424c163d1c70
nohjunh <nsh042777@naver.com>
2022-09-24 15:23:09 +0900

    facebook login

--- app/src/main/java/com/example/howistagram_f16/LoginActivity.kt
+++ app/src/main/java/com/example/howistagram_f16/LoginActivity.kt
@@ -1,19 +1,31 @@
 package com.example.howistagram_f16
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import android.widget.Toast
-import androidx.annotation.Nullable
+import androidx.appcompat.app.AppCompatActivity
 import com.example.howistagram_f16.databinding.ActivityLoginBinding
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.*
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
+import java.util.Arrays.asList
+
 
 class LoginActivity : AppCompatActivity() {
     // 중요 !! 여기서는 activity_login.xml을 사용하므로 ActivityLoginBinding으로 설정함
@@ -22,6 +34,8 @@ class LoginActivity : AppCompatActivity() {
     var auth: FirebaseAuth? = null
     var googleSignInClient: GoogleSignInClient? = null
     var GOOGLE_LOGIN_CODE = 9001
+    var callbackManager : CallbackManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -35,11 +49,37 @@ class LoginActivity : AppCompatActivity() {
             //First Step
             googleLogin()
         }
+
+        binding.facebookLoginButton.setOnClickListener {
+            //First step
+            facebookLogin()
+        }
+
         var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
         googleSignInClient = GoogleSignIn.getClient(this, gso)
+        //printHashKey() //해쉬값 가져오는건 주석처리 (해쉬값이 필요할때만 풀어서 사용)]
+
+        callbackManager = CallbackManager.Factory.create()
+    }
+
+    fun printHashKey() {
+        try {
+            val info: PackageInfo = packageManager
+                .getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val hashKey = String(Base64.encode(md.digest(), 0))
+                Log.i("TAG", "printHashKey() Hash Key: $hashKey")
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e("TAG", "printHashKey()", e)
+        } catch (e: Exception) {
+            Log.e("TAG", "printHashKey()", e)
+        }
     }
 
     fun googleLogin() {
@@ -47,8 +87,44 @@ class LoginActivity : AppCompatActivity() {
         startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
     }
 
+    fun facebookLogin(){
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, listOf("public_profile", "email"))
+
+        LoginManager.getInstance()
+            .registerCallback(callbackManager, object : FacebookCallback<LoginResult>{
+                override fun onSuccess(result: LoginResult?) {
+                    //second step
+                    handleFacebookAccessToken(result?.accessToken)
+                }
+
+                override fun onCancel() {
+                }
+
+                override fun onError(error: FacebookException?) {
+                }
+
+            })
+    }
+
+    fun handleFacebookAccessToken(token : AccessToken?){
+        var credential = FacebookAuthProvider.getCredential(token?.token!!)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    // Third step
+                    // Login
+                    moveMainPage(task.result?.user)
+                }else{
+                    // Show the error message
+                    Toast.makeText(this, task.exception!!.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        callbackManager?.onActivityResult(requestCode, resultCode,data)
         // google login
         if(requestCode == GOOGLE_LOGIN_CODE){
             var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data!!)
@@ -61,11 +137,10 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    fun firebaseAuthWithGoogle(account:GoogleSignInAccount?){
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?){
         var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
         auth?.signInWithCredential(credential)
-            ?.addOnCompleteListener {
-                    task ->
+            ?.addOnCompleteListener { task ->
                 if(task.isSuccessful){
                     // Login
                     moveMainPage(task.result?.user)
@@ -77,9 +152,11 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun signinAndSignup(){
-        auth?.createUserWithEmailAndPassword(binding.emailEdittext.text.toString(), binding.passwordEdittext.text.toString())
-            ?.addOnCompleteListener {
-                    task ->
+        auth?.createUserWithEmailAndPassword(
+            binding.emailEdittext.text.toString(),
+            binding.passwordEdittext.text.toString()
+        )
+            ?.addOnCompleteListener { task ->
                 if(task.isSuccessful){
                     // Creating a user account
                     moveMainPage(task.result?.user)
@@ -94,9 +171,11 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun signinEmail(){
-        auth?.signInWithEmailAndPassword(binding.emailEdittext.text.toString(), binding.passwordEdittext.text.toString())
-            ?.addOnCompleteListener {
-                    task ->
+        auth?.signInWithEmailAndPassword(
+            binding.emailEdittext.text.toString(),
+            binding.passwordEdittext.text.toString()
+        )
+            ?.addOnCompleteListener { task ->
                 if(task.isSuccessful){
                     // Login
                     moveMainPage(task.result?.user)
@@ -107,7 +186,7 @@ class LoginActivity : AppCompatActivity() {
             }
     }
 
-    fun moveMainPage(user:FirebaseUser?){
+    fun moveMainPage(user: FirebaseUser?){
         if(user!=null){
             startActivity(Intent(this, MainActivity::class.java))
         }
