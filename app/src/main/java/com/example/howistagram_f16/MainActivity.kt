5fcc4d25d3f80f9dd83847699c27ca9d335acdb6
nohjunh <nsh042777@naver.com>
2022-09-24 16:09:24 +0900

    main navigation toolbar

--- app/src/main/java/com/example/howistagram_f16/MainActivity.kt
+++ app/src/main/java/com/example/howistagram_f16/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.howistagram_f16
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.howistagram_f16.databinding.ActivityMainBinding
+import com.example.howistagram_f16.navigation.AlarmFragment
+import com.example.howistagram_f16.navigation.DetailViewFragment
+import com.example.howistagram_f16.navigation.GridFragment
+import com.example.howistagram_f16.navigation.UserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(binding.root)
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_home ->{
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search ->{
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,gridFragment).commit()
+                return true
+            }
+            R.id.action_add_photo ->{
+                return true
+            }
+            R.id.action_favorite_alarm ->{
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,alarmFragment).commit()
+                return true
+            }
+            R.id.action_account ->{
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,userFragment).commit()
+                return true
+            }
+        }
+        return false
     }
 }
---
+++ app/src/main/java/com/example/howistagram_f16/navigation/AlarmFragment.kt
@@ -0,0 +1,20 @@
+package com.example.howistagram_f16.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.howistagram_f16.R
+
+class AlarmFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm, container, false)
+
+        return view
+    }
+}
